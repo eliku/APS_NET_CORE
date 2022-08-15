a71ddc2a0ccfe4543e3cc41c4c13d8652cd2496d
GeekBrains <kubasovae@mail.ru>
2022-08-16 00:00:28 +0400

    begin

---
+++ Timesheets/Timesheets/Timesheets/Controllers/ContractsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheets.Controllers
+{
+    [ApiController]
+    [Route("[api/contracts]")]
+    public class ContractsController : ControllerBase
+    {
+        private readonly ILogger<ContractsController> _logger;
+
+        public ContractsController(ILogger<ContractsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Timesheets/Timesheets/Timesheets/Model/clEmployees.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheets.Model
+{
+    public class clEmployees
+    {
+        private int _id;
+        private string _fio;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string FIO
+        { 
+            get { return _fio; }
+            set { _fio = value; }
+        }
+    }
+}
